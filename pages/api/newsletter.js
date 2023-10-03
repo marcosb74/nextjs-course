90de2c95495378bb9bbce61a3f8ee6ebb80f51fc
as-takeba <markos.de.vl@gmail.com>
2023-10-03 12:37:57 -0300

    first endpoint and validation

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,11 +1,13 @@
 function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
-    if (!userEmail || userEmail.includes("@")) {
+
+    if (!userEmail || !userEmail.includes("@")) {
       res.status(422).json({ message: "invalid data" });
       return;
     }
     console.log(userEmail);
+    res.status(201).json({ message: "Email OK!" });
   }
 }
 export default handler;
