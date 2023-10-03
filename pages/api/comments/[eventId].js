f01b9b9175c0f28aa5224f50232b4ac5989e6403
as-takeba <markos.de.vl@gmail.com>
2023-10-03 14:20:41 -0300

    new router for eventid

---
+++ pages/api/comments/[eventId].js
@@ -0,0 +1,37 @@
+function handler(req, res) {
+  const eventId = req.query.eventId;
+
+  if (req.method === "POST") {
+    // add server side Validation
+    const { email, name, text } = req.body;
+    if (
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !text ||
+      text.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid Input." });
+      return;
+    }
+    const newComments = {
+      id: new Date().toISOString(),
+      email,
+      name,
+      text,
+    };
+    console.log(newComments);
+
+    res.status(201).json({ message: "Data ok!", comment: newComments });
+  }
+  if (req.method === "GET") {
+    const dummyList = [
+      { id: "c1", name: "max", text: "text@test.com" },
+      { id: "c2", name: "maximiliano", text: "segundo@test.com" },
+    ];
+
+    res.status(200).json({ comments: dummyList });
+  }
+}
+
+export default handler;
